c957845f775a35674959663c4ee315bb091f1a5a
RUakkRU <93092648+RUakkRU@users.noreply.github.com>
2021-10-24 20:38:05 +0300

    Add files via upload

---
+++ fpm_proxy.php
@@ -0,0 +1,73 @@
+<?php
+use Swoole\Websocket\Server;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Swoole\Coroutine\FastCGI\Proxy;
+$documentRoot = '/var/www/html';
+$server = new  Swoole\Websocket\Server( "127.0.0.1" , 8080, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+$fpmProxy = new Swoole\Coroutine\FastCGI\Proxy('127.0.0.1:9000', $documentRoot);
+
+$server->set([
+'user' => 'www-data',
+'group' => 'www-data',
+'open_http2_protocol' => true ,
+'ssl_cert_file' =>  '/etc/ssl/certs/localhost.crt',
+'ssl_key_file' =>  '/etc/ssl/private/localhost.key',
+'http_compression_level' => 5,
+'worker_num' => swoole_cpu_num() * 2,
+'http_parse_cookie' => false,
+'http_parse_post' => false,
+'document_root'  => $documentRoot,
+'http_compression_level' => 5,
+//'enable_static_handler' => true ,
+//  static resource paths
+//'static_handler_locations'  => ['/css','/img'],
+]);
+$server->on("WorkerStart", function($server, $workerId)
+{
+    echo "Worker Started: $workerId\n";
+});
+$server->on('Request', function( $request,  $response) use ($fpmProxy){ 
+$uri = $request->server['request_uri'];
+  echo "$uri\n";
+  if(preg_match('/.php/', $uri))
+  $uri = '/.php';
+  echo "$uri\n";
+  if(preg_match('/.css/', $uri))
+  $uri = '/.css';
+  echo "$uri\n";
+  if(preg_match('/.webp/', $uri))
+  $uri = '/.webp';
+  echo "$uri\n";
+     switch($uri) 
+        {
+         case '/.php':
+         $fpmProxy->pass($request, $response);
+         break;
+         case '/.css':
+            $css = $request->server['request_uri'];
+            $response -> header ( "Cache-Control" , "public" );
+            $response -> Header( 'Cache-Control' , 'max-age=31536000');
+            $response->sendfile($css);
+            break;
+            case '/.webp':
+                $img = $request->server['request_uri'];
+                $response -> header ( "Cache-Control" , "public" );
+                $response -> Header( 'Cache-Control' , 'max-age=31536000');
+                $response->sendfile($img);
+                break;
+                case '/manifest.json':
+                    $response -> header ( "Cache-Control" , "public" );
+                    $response -> Header( 'Cache-Control' , 'max-age=31536000');
+                    $response->sendfile('/var/www/html/manifest.json');
+                    break;
+                    case '/robots.txt':
+                        $response -> header ( "Cache-Control" , "public" );
+                        $response -> Header( 'Cache-Control' , 'max-age=31536000');
+                        $response->sendfile('/var/www/html/robots.txt');
+                        break;                     
+                    }   
+       });
+$server->on("message", function ($server) {
+});
+$server->start();
